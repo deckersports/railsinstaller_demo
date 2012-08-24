79ba525e49be907427793febc3c200289cccdffb
webmaster@deckersports.biz <webmaster@deckersports.biz>
2012-08-24 14:18:58 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '659527a58f4853a0f5f7b17cdac62764526cca4d38a9b335eab671d53b58d2f88d5fe70058424084a7734590bd998e9bbd3bcfefee5a69f8d2f8e9246ec0b33e'
